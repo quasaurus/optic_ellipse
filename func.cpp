9edb787e87ee9c2b91eb3d11d691d6bbb3dd1b0d
Sabine Akhmedova <97114415+quasaurus@users.noreply.github.com>
2026-05-18 20:36:33 +0300

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,71 +1,112 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
+#include <iomanip>
+
 struct Point {
     double x, y;
-    Point(double x = 0, double y = 0) {};
+    Point(double x = 0, double y = 0) : x(x), y(y) {};
 };
-// мяу мяу
-class Ellipce {
+
+double epsilon = 10.0 * std::numeric_limits<double>::epsilon();
+
+class Ellipse {
 private:
     double x0, y0, a, b, c;
     Point f1, f2;
 public:
-    Ellipce(double x0, double y0, double a, double b) {
+    Ellipse(double x0, double y0, double a, double b) : x0(x0), y0(y0), a(a), b(b) {
         c = std::sqrt(std::abs(a * a - b * b));
         if (a > b) {
-            f1 = Point(x0 - c, y0);
-            f2 = Point(x0 + c, y0);
+            f1 = Point(-c, 0);
+            f2 = Point(c, 0);
         }
         else {
-            f1 = Point(x0, y0 - c);
-            f2 = Point(x0, y0 + c);
+            f1 = Point(0, -c);
+            f2 = Point(0, c);
         }
     }
-    bool inEllipce(Point m) const {
-        if (pow(m.x - x0, 2) / pow(a, 2) + pow(m.y - y0, 2) / pow(b, 2) < 1)return true;
+    bool onEllipse(Point& m) const {
+        if (fabs(m.x * m.x / (a * a) + m.y * m.y / (b * b) - 1) < epsilon)return true;
         return false;
     }
-    bool onEllipce(Point m) const {
-        if (pow(m.x - x0, 2) / pow(a, 2) + pow(m.y - y0, 2) / pow(b, 2) == 1)return true;
-        return false;
+    Point fromNormalToShtrih(Point& a) {
+        return Point(a.x - x0, a.y - y0);
     }
-    double fromPointToLine(Point c, Point l, Point n) {
-        double A = l.y - n.y;
-        double B = n.x - l.x;
-        double C = n.y * l.x - n.x * l.y;
-        return (std::abs(A * c.x + B * c.y + C) / std::sqrt(A * A + B * B));
+    Point fromShtrihToNormal(Point& a) {
+        return Point(a.x + x0, a.y + y0);
     }
-    Point pointPer(Point m, double k) {
-        double A = 1 / (a * a) + (k * k) / (b * b);
-        double B = 2 * (m.x - x0) / (a * a) + (k * m.y - k * y0) / (b * b);
-        double C = (m.x - x0) * (m.x - x0) / (a * a) + (m.y - y0) * (m.y - y0) / (b * b) - 1;
-        double dis = B * B - 4 * A * C;
-        if (dis < 0)return Point(m.x + 1, m.y + k); /////
-        double t1 = (- B + std::sqrt(dis)) / (2 * A);
-        double t2 = (- B - std::sqrt(dis)) / (2 * A);
-        double t = std::min(t1, t2);
-        if (dis == 0)return Point(m.x + t1, m.y + k * t1);
-        if (t1 < 0 && t2 < 0)return Point(m.x + 1, m.y + k);////
-        if (t1 < 0 && t2 >= 0)return Point(m.x + t2, m.y + k * t2);
-        if (t1 > 0 && t2 <= 0)return Point(m.x + t1, m.y + k * t1);
-        return Point(m.x + t, m.y + k * t);
+    Point pointInters(Point& m, double& angle) { //см. комментарий на строке 75
+        double dx = std::cos(angle);
+        double dy = std::sin(angle);
+        double A = dx * dx / (a * a) + dy * dy / (b * b);
+        double B = 2 * ((m.x * dx / (a * a)) + m.y * dy / (b * b));
+        double C = m.x * m.x / (a * a) + m.y * m.y / (b * b) - 1;
+        double t1, t2, t = 0;
+        double max = std::max(B * B, fabs(4 * A * C)); //зачем модуль для B^2, квадрат же всегда положительный
+        if (max < epsilon) { 
+            t = 0;
+            return m;
+        }
+        double norm_D = (B * B - 4 * A * C) / max;
+        if (norm_D > epsilon) {
+            double D = norm_D * max; //я бы тут сделала D = B * B - 4AC, потому что сначала деля на макс, а потом умножая, набегает погрешность
+            t1 = ((-B + sqrt(D)) / (2 * A));
+            t2 = ((-B - sqrt(D)) / (2 * A));
+            if (fabs(t1 - t2) > epsilon) {
+                if (t1 > epsilon && t2 < epsilon) t = t1;
+                else if (t1 < epsilon && t2 > epsilon) t = t2; // это невозможный случай, t2 всегда больше t1. См. строку 41, у нас А всегда больше нуля
+                else if (t1 > epsilon && t2 > epsilon) t = std::min(t1, t2);
+                else if ((t1 < epsilon && t2 > -epsilon) || (t1 > -epsilon && t2 < epsilon)) t = 0; //а нет такого вайба, что этот случай мы рассматриваем в строке 46?
+                else t = 1;
+            }
+            else if (t1 < -epsilon) t = 1; //вроде это тоже лишнее
+            else t = std::min(t1, t2); //это лишнее, мы покрываем это в строках 55-60
+        }
+        if (norm_D < -(epsilon)) {
+            t = 1;
+        }
+        if (fabs(norm_D) < epsilon) {
+            t = -B / (2 * A);
+            if (fabs(t) < epsilon) t = 0;
+            else if (t < -epsilon) t = 1;
+        }
+        return Point(m.x + dx * t, m.y + dy * t);
     }
-    Point reflect(Point m, double k) {
-        Point p = pointPer(m, k);
-        double A = p.x / (a * a);
-        double B = p.y / (b * b);
-        double t = (A * (p.x - m.x) - B * (p.y - m.y)) / (B * B - A * A);
+    Point reflect(Point& m, double& angle) //а нет такого, что в эту функцию хорошо бы и эллипс передавать (и делать ее friend функцией к классу эллипс). У нас в задаче эллипс конечно один, но...
+    {
+        Point p = pointInters(m, angle); 
+        if (fabs(m.x - p.x) < epsilon && fabs(m.y - p.y) < epsilon) return m; //может здесь сделать расстояние от точки пересечения меньше эпсилон (квадратный корень из суммы квадратов), а не отдельно эпсилон по вертикали и горизонтали?
+        double xn = -b * b / p.x;
+        double yn = a * a / p.y;
+        double xk = p.x / (a * a);
+        double yk = p.y / (b * b);
+        double t = (p.x - m.x) / (xk - xn);
         Point l;
-        l.x = 2 * A * t + 2 * p.x - m.x;
-        l.y = m.y + A * (l.x - m.x) / B;
+        l.x = m.x + 2 * xk * t;
+        l.y = m.y + 2 * yk * t;
         return l;
     }
-    void resh(Point m, double k, double& d1, double& d2, double& x, double& y) {
-        Point p = pointPer(m, k);
-		Point l = reflect(m, k);
-		d1 = fromPointToLine(f1, p, l);
-		d2 = fromPointToLine(f2, p, l);
+    double distFromPointToLine(Point& f, Point& p, Point& l) {
+        double A = p.y - l.y;
+        double B = l.x - p.x;
+        double C = l.y * p.x - l.x * p.y;
+        double t = ((l.y - p.y) * (f.y - p.y) + (l.x - p.x) * (f.x - p.x));
+        if (t > 0) return (std::abs(A * f.x + B * f.y + C) / std::sqrt(A * A + B * B));
+        return std::sqrt((p.x - f.x) * (p.x - f.x) + (p.y - f.y) * (p.y - f.y));
+    }
+    void solution(Point M, double& angle, double& d1, double& d2, double& x, double& y) {  //тут надо по идее М ссылкой передавать, а то ты его копией передаешь, а потом меняешь
+        M = fromNormalToShtrih(M);
+        Point p = pointInters(M, angle);
+        Point l = reflect(M, angle);
+        if (onEllipse(p) || onEllipse(M)) {
+            d1 = distFromPointToLine(f1, p, l);
+            d2 = distFromPointToLine(f2, p, l);
+        }
+        else {
+            d1 = distFromPointToLine(f1, M, l);
+            d2 = distFromPointToLine(f2, M, l);
+        }
+        p = fromShtrihToNormal(p); // как-то очень загадачно. Мы создаем точку P внутри функции и как-то ее меняем там же. Как нам снаружи от функции узнать что-то про P, если функция возвращает void?
         x = p.x;
         y = p.y;
     }
@@ -74,17 +115,16 @@ public:
 int main()
 {
     double x0, y0, a, b, xi, eta;
-     int m;
+    int m;
     std::cin >> x0 >> y0 >> a >> b;
     std::cin >> xi >> eta;
     std::cin >> m;
     Point M(xi, eta);
-    Ellipce ellipce(x0, y0, a, b);
+    Ellipse ellipse(x0, y0, a, b);
     for (int j = 0; j < m; ++j) {
-        double k = atan(2 * asin(-1) * j / m);
         double d1 = 0, d2 = 0, x, y;
-        ellipce.resh(M, k, d1,d2, x, y);
+        double angle = 2 * j * acos(-1) / m; //давай где-нибудь константой обозначим пи а то его прям в формулу ставить в виде арккосинуса(-1) как-то не комильфо...
+        ellipse.solution(M, angle, d1, d2, x, y);
         std::cout << x << " " << y << " " << d1 << " " << d2 << std::endl;
     }
-    
 }
